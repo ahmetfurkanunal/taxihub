ef8813de00ccb009746faa3f09ede05abdf7ce18
Ahmet Furkan Ünal <afurkanx33@gmail.com>
2025-12-04 11:03:58 +0300

    Add gateway service and docker-compose to project

---
+++ gateway/jwt.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("supersecretkey123")
+
+func JWTAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		_, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+			return secretKey, nil
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
